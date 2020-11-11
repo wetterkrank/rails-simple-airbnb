fbcad13d68857be7468266f269e6f3a0e4b301ca
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-11 17:04:03 +0100

    Just a flats cards draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database now..."
+Flat.destroy_all
+puts "Database clean ✅"
+
+5.times do 
+  Flat.create!(
+    name: Faker::Movies::StarWars.planet,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.yoda,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(1..5)
+  )  
+end
+
+puts "Done! Created #{Flat.count} flats."
